f473b1215a675af626fd1538de8e423b5e47840c
Zaana A <zcesfal@ucl.ac.uk>
2019-10-23 15:43:59 +0100

    a cutesy graph (commit 5)

--- testingthings.R
+++ testingthings.R
@@ -7,3 +7,15 @@ anothervar <- 25 * 6
 print (anothervar)
 
 print (sumz + anothervar)
+
+
+#Using random codes
+
+# Define the cars vector with 5 values
+cars <- c(1, 3, 6, 4, 9)
+
+# Graph cars using blue points overlayed by a line 
+plot(cars, type="o", col="blue")
+
+# Create a title with a red, bold/italic font
+title(main="Autos", col.main="red", font.main=4)
